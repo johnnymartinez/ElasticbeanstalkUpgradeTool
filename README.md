b8aec2231565712e0af8846baf6e930fdc10a63f
johnnymartinez <69644947+johnnymartinez@users.noreply.github.com>
2020-08-13 13:15:49 -0600

    Removed Log files and sanitized code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Platform Update Tool & enhanced Reporting
 
-This tool is used in conjunction with AWS Managed Update releases for Elasticbeanstalk platforms. This tool will compare the existing version of node on each server against latest possible version of node that is compatible with the latest platform. If the existing version of node.js is no longer available this tool will upgrade the platform while upgrading node.js to the next minor release available.
+This tool is used in conjunction with AWS Managed Update releases for Elasticbeanstalk platforms. This tool will compare the existing version of node on each server against latest possible version of node that is compatible with the latest platform. If the existing version of node.js is antiqiated this tool will upgrade the Elasticbeanstalk platform while upgrading node.js to the next minor that is compatible with AWS Elasticbeanstalk platform.
 
 ## Prerequisites
 Python 3
@@ -9,8 +9,9 @@ Python 3
 ![Architecture](./Architecture.png)
 
 ## Parameters
-
-### NEXT SECTION
+### -Prod
+### -Non-Prod
+### -All-Servers
 
 ## NEXT SECTION
 
--- lib/abortable_operation.py
+++ lib/abortable_operation.py
@@ -3,13 +3,12 @@ import time
 import json
 
 
-def abortable_operation(envName):
+def abortable_operation_upgrade(envName):
 
     client = boto3.client('elasticbeanstalk')
     print('\n\nConnecting')
 
-    print('\n\nChecking to see if ', envName,
-          ' is in a ready state.')
+    print('\n\nChecking for ready status on environment -', envName)
 
     while True:
 
@@ -21,11 +20,11 @@ def abortable_operation(envName):
         status = abortable_response['Environments'][0]['Status']
 
         if status == 'Ready':
-            print("\n\n" + envName + ' is now ready!')
+            print(envName, 'is now ready!')
             break
         else:
-            time.sleep(10)
-            # print('''
-            # -----------------------------------------------------------------------------------------
-            #     Sleeping for another 10 seconds... waiting for configuration changes to finish up.
-            # -----------------------------------------------------------------------------------------''')
+            print('''
+            -----------------------------------------------------------------------------------------
+                Sleeping for four minutes... waiting for immutable upgrade to finish.
+            -----------------------------------------------------------------------------------------''')
+            time.sleep(240)
